cf9675947a6e33def5deb1617655ac0f8ca93250
Guglielmo <guglielmo.donato04@gmail.com>
2026-01-04 23:16:45 +0100

    toast or error link expired

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/shared/UI/Navbar";
 import Footer from "@/shared/UI/Footer";
 import { useEffect } from "react";
-import { Outlet, useNavigation, redirect, type LoaderFunctionArgs } from "react-router";
+import { Outlet, useNavigation, redirect, type LoaderFunctionArgs, useSearchParams } from "react-router";
 import { Spinner } from "@/components/ui/spinner";
 import { authKeys, authQueryOptions } from "@/api/queries/authQueries";
 import { supabase } from "@/lib/supabase";
 import { useQueryClient, QueryClient } from "@tanstack/react-query";
 import { authService } from "@/api/services/authApi";
 import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
 
 export const rootLoader = (queryClient: QueryClient) => async ({ request }: LoaderFunctionArgs) => {
     const url = new URL(request.url);
@@ -25,8 +26,9 @@ export const rootLoader = (queryClient: QueryClient) => async ({ request }: Load
 }
 
 const RootLayout = () => {
-    const navigation = useNavigation();
-    const queryClient = useQueryClient();
+  const navigation = useNavigation();
+  const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
 
     useEffect(() => {
         const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
@@ -37,21 +39,34 @@ const RootLayout = () => {
         return () => subscription.unsubscribe();
     }, [queryClient]);
 
+  useEffect(() => {
+    const hashParams = new URLSearchParams(window.location.hash.substring(1));
+    const errorMsg = hashParams.get("error_description");
+
+    if (errorMsg) {
+      toast.error(decodeURIComponent(errorMsg.replace(/\+/g, " ")));
+
+      if (window.location.hash) {
+        window.history.replaceState(null, "", window.location.pathname);
+      }
+    }
+  }, [searchParams]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-            <Toaster richColors />
-
+      <Toaster richColors />
       <main className="flex-grow">
-        
-        {navigation.state === "loading" && <div className="flex items-center justify-center gap-4 py-8">
+        {navigation.state === "loading" && (
+          <div className="flex items-center justify-center gap-4 py-8">
             <Spinner variant="primary" size="lg" />
-          </div>}
+          </div>
+        )}
         <Outlet />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default RootLayout
