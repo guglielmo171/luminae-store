944d622cd7972afc01e7fcb06007c710412ea45d
Guglielmo <guglielmo.donato04@gmail.com>
2026-01-18 21:08:04 +0100

    vite config loaded by env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+
+const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), 'VITE_');
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -39,7 +43,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: env.VITE_API_URL,
         changeOrigin: true,
       }
     }
