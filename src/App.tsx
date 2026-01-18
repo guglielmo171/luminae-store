6c3970ba25aefbd18f5b6a4e1f709fd64e10c94e
Guglielmo <guglielmo.donato04@gmail.com>
2026-01-18 22:19:25 +0100

    Lazy load for heavy components
    Lazy load for Pages like products and categories and dashboard admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import RootLayout, { rootLoader } from './pages/RootLayout';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ProductsPage,{loader as productsLoader} from './pages/ProductsPage';
-import ProductPage from './pages/ProductPage';
-import LoginPage from './pages/LoginPage';
-import ProfilePage from './pages/ProfilePage';
-import UpdatePasswordPage from './pages/UpdatePasswordPage';
+import {loader as productsLoader} from './pages/ProductsPage';
+// import ProductPage from './pages/ProductPage';
+// import LoginPage from './pages/LoginPage';
+// import ProfilePage from './pages/ProfilePage';
+// import UpdatePasswordPage from './pages/UpdatePasswordPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { handleGlobalError } from './api/utils/errorHandler';
 import AdminLayout, { adminLoader } from './pages/admin/AdminLayout';
-import AdminProductsPage from './pages/admin/AdminProductsPage';
+// import AdminProductsPage from './pages/admin/AdminProductsPage';
 import UnauthorizedPage from './pages/UnauthorizedPage';
-import AdminCategoriesPage from './pages/admin/AdminCategoriesPage';
-import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+// import AdminCategoriesPage from './pages/admin/AdminCategoriesPage';
+// import AdminDashboardPage from './pages/admin/AdminDashboardPage';
 import ErrorPage from './pages/ErrorPage';
+import { lazy } from 'react';
 export const queryClient = new QueryClient({
   defaultOptions:{
     mutations:{
@@ -23,6 +24,18 @@ export const queryClient = new QueryClient({
     }
   }
 });
+
+// Lazy pages
+const HomePage=lazy(()=>import("./pages/HomePage"))
+const LoginPage=lazy(()=>import("./pages/LoginPage"))
+const ProfilePage=lazy(()=>import("./pages/ProfilePage"))
+const UpdatePasswordPage=lazy(()=>import("./pages/UpdatePasswordPage"))
+const ProductsPage=lazy(()=>import("./pages/ProductsPage"))
+const ProductPage=lazy(()=>import("./pages/ProductPage"))
+const AdminDashboardPage=lazy(()=>import("./pages/admin/AdminDashboardPage"))
+const AdminProductsPage=lazy(()=>import("./pages/admin/AdminProductsPage"))
+const AdminCategoriesPage=lazy(()=>import("./pages/admin/AdminCategoriesPage"))
+
 function App() {
 
   const router = createBrowserRouter([
